ad7bfd10912a2d1dfe3f8c8b4188dc2d3fe88869
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-03-09 15:03:38 -0400

    new constructor and function to find num alive neighbords

--- src/colony.hpp
+++ src/colony.hpp
@@ -7,11 +7,14 @@ class Colony {
 public:
 	// Constructors
 	Colony(int screen_width_in, int screen_height_in, int cell_size);
+	Colony(const std::vector<std::vector<int>>& bit_map);
 
 	// Getters
 	int get_num_rows();
 	int get_num_cols();
 	int get_cell_at(int row, int column);
+
+	int find_num_alive_neighbors(int row, int column);
 
 	// Setters
 	void set_cell_at(int row, int column, int value);
