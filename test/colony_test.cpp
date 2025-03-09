421fbcea8efff9ddef4fedb46b58e15e04349811
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-03-08 22:07:07 -0500

    test basic constructor for colony

--- test/colony_test.cpp
+++ test/colony_test.cpp
@@ -6,3 +6,15 @@ int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
+TEST(Constructor, basic) {
+	Colony colony(800, 400);
+	ASSERT_EQ(colony.getNumCols(), 800/2);
+	ASSERT_EQ(colony.getNumRows(), 400/2);
+	
+	for (int i = 0; i < colony.getNumRows(); i++) {
+		for (int j = 0; j < colony.getNumCols(); j++) {
+			ASSERT_EQ(colony.getCellAt(i, j), 0);
+		}
+	}
+}
