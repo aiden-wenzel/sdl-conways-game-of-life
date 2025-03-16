12112b8027c45f3730bde3d7b42dc0824eddd5e6
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-03-16 01:31:24 -0400

    add mouse_unit_test

---
+++ test/mouse_test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "mouse.hpp"
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+TEST(Mouse, mouse_pos_to_index) {
+	std::pair<int, int> mouse_pos = {15, 23};
+	float cell_size = 10;
+	std::pair<int, int> index = {2, 1};
+	ASSERT_EQ(mouse_to_index(mouse_pos, cell_size), index);
+}
