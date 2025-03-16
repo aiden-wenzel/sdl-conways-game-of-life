1e48e538bc24c2196e81d6f6a5fe195e59bc8536
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-03-16 15:05:38 -0400

    write render_mouse_cell and draw_cells

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -1,5 +1,6 @@
 #include <SDL3/SDL.h>
 #include <utility>
+#include <cmath>
 
 #include "mouse.hpp"
 
@@ -9,4 +10,22 @@ std::pair<int, int> get_mouse_pos(SDL_Event* event) {
 
 std::pair<int, int> mouse_to_index(const std::pair<int, int>& mouse_pos, float cell_size) {
 	return {mouse_pos.second/cell_size, mouse_pos.first/cell_size};
+}
+
+void render_mouse_cell(SDL_Renderer* renderer, const std::pair<int, int>& mouse_pos, float cell_size) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 129, 0);
+
+	float x = floor(mouse_pos.second/cell_size);
+	float y = floor(mouse_pos.first/cell_size);
+
+	x *= cell_size;
+	y *= cell_size;
+
+	SDL_FRect cell = {y, x, cell_size, cell_size};
+	SDL_RenderFillRect(renderer, &cell);
+}
+
+void draw_cells(SDL_Renderer* renderer, const std::pair<int, int>& mouse_pos, Colony* colony, float cell_size) {
+	std::pair<int, int> indicies = mouse_to_index(mouse_pos, cell_size);
+	colony->set_cell_at(indicies.first, indicies.second, 1);
 }
--- src/mouse.hpp
+++ src/mouse.hpp
@@ -1,6 +1,9 @@
 #include <SDL3/SDL.h>
 #include <utility>
 
-std::pair<int, int> get_mouse_pos(SDL_Event* event);
+#include "colony.hpp"
 
+std::pair<int, int> get_mouse_pos(SDL_Event* event);
 std::pair<int, int> mouse_to_index(const std::pair<int, int>& mouse_pos, float cell_size);
+void render_mouse_cell(SDL_Renderer* renderer, const std::pair<int, int>& mouse_pos, float cell_size); 
+void draw_cells(SDL_Renderer* renderer, const std::pair<int, int>& mouse_pos, Colony* colony, float cell_size);
