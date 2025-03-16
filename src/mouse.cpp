d272fe0f2a661ac42658e0dd84364797fd170b57
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-03-16 01:31:12 -0400

    add function to convert from mouse to index

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -4,3 +4,7 @@
 std::pair<int, int> get_mouse_pos(SDL_Event* event) {
 	return {event->motion.x, event->motion.y};
 }
+
+std::pair<int, int> mouse_to_index(const std::pair<int, int>& mouse_pos, float cell_size) {
+	return {mouse_pos.second/cell_size, mouse_pos.first/cell_size};
+}
--- src/mouse.hpp
+++ src/mouse.hpp
@@ -2,3 +2,5 @@
 #include <utility>
 
 std::pair<int, int> get_mouse_pos(SDL_Event* event);
+
+std::pair<int, int> mouse_to_index(const std::pair<int, int>& mouse_pos, float cell_size);
